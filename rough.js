b00de89eb548c1538a36835e8e16a708affb2cca
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-06-27 15:26:58 +0530

    Redux_with_React_Basic(3)

---
+++ rough.js
@@ -0,0 +1,32 @@
+// function reducer(state = 1999, action) {
+    //   switch (typeof state) {
+    //     case "undefined":
+    //       return (state = 0);
+
+    //     default:
+    //       break;
+    //   }
+
+    //   switch (action.type) {
+    //     case "INCREMENT":
+    //       return state + 1;
+
+    //     case "DECREMENT":
+    //       return state - 1;
+
+    //     default:
+    //       return state;
+    //   }
+    // }
+    // expect(reducer(0, { type: "INCREMENT" })).toBe(1);
+    // expect(reducer(1, { type: "INCREMENT" })).toBe(2);
+
+    // expect(reducer(1, { type: "DECREMENT" })).toBe(0);
+    // expect(reducer(2, { type: "DECREMENT" })).toBe(1);
+
+    // expect(reducer(1, { type: "SOMETHINGELSE" })).toBe(1);
+
+    // expect(reducer(undefined, {})).toBe(1999);
+
+    // console.log("All Test has been Passed");
+    // console.log(Redux);
